d5c17811a70ac45c317b5f26bffb8d4f8ff0b2e5
HitoTright <915840223@qq.com>
2018-11-26 16:07:58 +0800

    jpa 测试完毕

---
+++ src/main/java/top/hitotright/spdemo/domain/UserRepository.java
@@ -0,0 +1,14 @@
+package top.hitotright.spdemo.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
+
+    User findByNameAndAge(String name, Integer age);
+
+    @Query("from User u where u.name=:name") //这里事hsq和sql不一样
+    User findUser(@Param("name") String name);
+}
